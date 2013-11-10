65e879b8087415a5df0d5d4176f12c3ce8ff3c8d
fwhetstone <fenton.whetstone@gmail.com>
2013-11-10 16:33:08 -0700

    More layouts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+	
+	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
 	describe "Home page" do
 
@@ -11,7 +13,7 @@ describe "Static Pages" do
 
 		it "should have the title 'Home'" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => " | Home")
+			page.should have_selector('title', :text => "#{base_title} | Home")
 		end
 	end
   
@@ -23,7 +25,7 @@ describe "Static Pages" do
 
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			page.should have_selector('title', :text => " | Help")
+			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 	end
   
@@ -35,7 +37,7 @@ describe "Static Pages" do
 
 		it "should have the title 'About'" do
 			visit '/static_pages/about'
-			page.should have_selector('title', :text => " | About")
+			page.should have_selector('title', :text => "#{base_title} | About")
 		end
 	end
   
@@ -48,7 +50,7 @@ describe "Static Pages" do
 
 		it "should have the title 'Contact'" do
 			visit '/static_pages/contact'
-			page.should have_selector('title', :text => " | Contact")
+			page.should have_selector('title', :text => "#{base_title} | Contact")
 		end
 	end
   
